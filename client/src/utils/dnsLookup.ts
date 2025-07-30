7bc9d10681614057de693e72a3d6a5cc1e000d2d
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-07-30 19:12:15 +0000

    corrigindo URL do endpoint da API

--- client/src/utils/dnsLookup.ts
+++ client/src/utils/dnsLookup.ts
@@ -1,5 +1,5 @@
 export function dnsLookup(dominio: string): Object {
-  const dados = fetch(`https://ideal-bassoon-gwjx6jjr7wj2vvp7-3000.app.github.dev/nslookup/${dominio}`)
+  const dados = fetch(`https://curly-space-computing-machine-7467w665wp52wrxp-3000.app.github.dev/nslookup/${dominio}`)
     .then((resposta) => resposta.json());
   return dados;
 }
--- client/src/utils/whoisLookup.ts
+++ client/src/utils/whoisLookup.ts
@@ -1,5 +1,5 @@
 export function whoisLookup(dominio: string): Object {
-  const dados = fetch(`https://ideal-bassoon-gwjx6jjr7wj2vvp7-3000.app.github.dev/whois/${dominio}`)
+  const dados = fetch(`https://curly-space-computing-machine-7467w665wp52wrxp-3000.app.github.dev/whois/${dominio}`)
     .then((resposta) => resposta.json());
   return dados;
 }
