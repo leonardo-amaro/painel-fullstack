6644ce2767d2b78b341f34fd4e53361f48350168
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-08-06 19:59:20 +0000

    atualizando endpoint da API no Railway

--- client/src/utils/dnsLookup.ts
+++ client/src/utils/dnsLookup.ts
@@ -1,5 +1,5 @@
 export function dnsLookup(dominio: string): Object {
-  const dados = fetch(`https://curly-space-computing-machine-7467w665wp52wrxp-3000.app.github.dev/nslookup/${dominio}`)
+  const dados = fetch(`https://nslookup-api-production.up.railway.app/nslookup/${dominio}`)
     .then((resposta) => resposta.json());
   return dados;
 }
--- client/src/utils/whoisLookup.ts
+++ client/src/utils/whoisLookup.ts
@@ -1,5 +1,5 @@
 export function whoisLookup(dominio: string): Object {
-  const dados = fetch(`https://curly-space-computing-machine-7467w665wp52wrxp-3000.app.github.dev/whois/${dominio}`)
+  const dados = fetch(`https://nslookup-api-production.up.railway.app/whois/${dominio}`)
     .then((resposta) => resposta.json());
   return dados;
 }
